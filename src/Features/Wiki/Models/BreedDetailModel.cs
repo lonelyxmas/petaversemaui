c68f633e4770cfd9a9b41113b657caa2c4fd1e29
Nguyễn Hoàng Phi Long <onithelord@gmail.com>
2023-04-21 12:52:56 +0700

    #8-feature-breed-detail-logic

---
+++ src/Features/Wiki/Models/BreedDetailModel.cs
@@ -0,0 +1,31 @@
+﻿namespace PetaverseMAUI;
+
+public partial class BreedDetailModel : BaseModel
+{
+    [ObservableProperty]
+    SpeciesPivotModel species;
+
+    [ObservableProperty]
+    string source;
+
+    [ObservableProperty]
+    string hairColor;
+
+    [ObservableProperty]
+    string hairLength;
+
+    [ObservableProperty]
+    WikiPageSizeEnum size;
+
+    [ObservableProperty]
+    double weight;
+
+    [ObservableProperty]
+    string longevity;
+
+    [ObservableProperty]
+    WikiPageSizeEnum energy;
+
+    [ObservableProperty]
+    string otherCharacterictics;
+}
